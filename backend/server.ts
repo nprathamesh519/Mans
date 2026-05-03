1f09179625cbfdb763e83fc325088a96a1d57de4
nprathamesh519 <nprathamesh519@gmail.com>
2026-05-04 00:09:16 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -69,10 +69,10 @@ async function startServer() {
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-  app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN || "*",
-    credentials: true
-  }));
+ app.use(cors({
+  origin: ["https://mans-oarw.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
 
   // SOCKET
   io.on('connection', (socket) => {
